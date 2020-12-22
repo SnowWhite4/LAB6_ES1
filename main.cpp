bbb4aaf1a7b91fb5d66488a0693245c0a50da1b6
ANNA SCARPA FALCE <anna.scarpafalce@studenti.unipd.it>
2020-12-22 19:49:46 +0100

    conclusione definitiva

--- main.cpp
+++ main.cpp
@@ -5,6 +5,13 @@
 #include <numeric>
 using namespace std;
 
+bool notNegative(double d)
+{
+	if (d >= 0)
+		return true;
+	return false;
+}
+
 vector<double> manipulate_vector(vector<double>& input_vector)
 {
 	vector<double> ret;
@@ -22,15 +29,17 @@ vector<double> manipulate_vector(vector<double>& input_vector)
 	double sum = accumulate(input_vector.begin(), input_vector.end(), 0.0);
 	ret.push_back(sum);   //somma degli elementi in ret
 
-	double sum1 = accumulate(origin.begin(), origin.end(), 0.0);
+	double sum1 = accumulate(origin.begin(), origin.end(), 0.0); 
 	ret.push_back(sum1);   //somma degli elementi della prima metà in ret
 
+	double num_not_negative = count_if(input_vector.begin(), input_vector.end(), notNegative);
+	ret.push_back(num_not_negative);   //conta numeri positivi
 
 	return ret;
 }
 int main()
 {
-	vector<double> v{ 0.5, 5, 2, 7, 0.1, 8.7, 5.1 }; //28.4
+	vector<double> v{ 0.5, 5, -2, 7, 0.1, 8.7, 5.1 }; //24.4
 	vector<double> manipulated = manipulate_vector(v);
 	for (int i = 0; i < manipulated.size(); i++)  //sort nel vettore di ritorno
 		cout << manipulated[i] << ", ";
